09160a2363139b7ffe637b121e6c425486af6c7a
Mike Ivanov <mika10.sti@gmail.com>
2022-11-07 16:20:07 +0100

    Add autoplay

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import ImageSlider from './Components/molecule/ImageSlider';
 import './Assets/CSS/main.scss';
 
 const App:React.FC = () => {
-  const slider = <ImageSlider width="80%" height="100%" />;
+  const slider = <ImageSlider width="80%" height="100%" isAutoPlay autoPlaySeconds={5} />;
   return (
     <div className="main">
       {slider}
